cbacf39fd8e1408b4a60887e2ef4b3602e9df6aa
Alice Gorius <alice.gorius@kedgebs.com>
2023-06-20 16:59:09 +0200

    homepage

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
+
+file = URI.open("https://asset.cloudinary.com/dkx9xgzon/1d393eaf478154cec48eb581cbb9ed87")
+clown1 = Clown.new(username: "Melissandre", user: User.last, category: "clown de mariage", localisation: "Paris", price: "40€/h")
+clown1.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+clown1.save
 
 Clown.create(username: "Melissandre", user: User.last, category: "clown de mariage", localisation: "Paris", price: "40€/h")
+
 Clown.create(username: "Alfred Penier", user: User.last, category: "clown d'anniversaire", localisation: "Avignon", price: "60€/h")
 Clown.create(username: "Jean Pascal", user: User.last, category: "clown d'hôpital", localisation: "Nice", price: "25€/h")
 
