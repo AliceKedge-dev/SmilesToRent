e2903f5ee97de1857e9ec30ce08774f6ae29a10a
Gaspard Lachaize <gaspard.lachaize@kedgebs.com>
2023-06-20 15:08:54 +0200

    j'ai rename le controller clown au pluriel

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  get "clown", to: "clown#index"
+  # get "/clowns", to: "clowns#index"
+  # get "/clowns/:id", to: "clowns#show"
+  resources :clowns
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
 end
--- test/controllers/clowns_controller_test.rb
+++ test/controllers/clowns_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class ClownControllerTest < ActionDispatch::IntegrationTest
+class ClownsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
