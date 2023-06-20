e8bbd18e369c93fe81a8f8ce32a779b513766e5e
Alice Gorius <alice.gorius@kedgebs.com>
2023-06-20 15:08:22 +0200

    home

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get "clown", to: "clown#index"
+  get "search", to: "search#search"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
