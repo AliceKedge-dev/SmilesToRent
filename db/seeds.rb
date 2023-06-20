d3a99037fc47245306faabeaa63d2515f1134ffb
Alice Gorius <alice.gorius@kedgebs.com>
2023-06-20 10:54:23 +0200

    seeds & gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,9 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+Clown.create!(username: "Melissandre", user: User.last)
+Clown.create!(username: "Queen C", user: User.last)
+Clown.create!(username: "Michel", user: User.last)
+
+User.create(email: "email@email.com", password: "password")
+User.create(email: "user@email.com", password: "userpassword")
