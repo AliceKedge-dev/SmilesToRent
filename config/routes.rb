c6a71176d46d2a0aabf0115604da1dfa996190e2
Alice Gorius <alice.gorius@kedgebs.com>
2023-06-23 17:01:00 +0200

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'bookings/index'
-  get 'bookings/show'
-  get 'bookings/create'
-  get 'bookings/new'
-  get 'bookings/edit'
-  get 'bookings/update'
-  get 'bookings/desroy'
   devise_for :users
   root to: "pages#home"
   resources :clowns do
